5e8dc376e314d9468ee8b03bfd45ba38ccbe856b
ivalina <ivalina757@gmail.com>
2022-11-22 18:32:52 +0200

    javascript elementary project solved

---
+++ javascript/Nov-2022/03.Conditions/speed.js
@@ -0,0 +1,16 @@
+function speed(input) {
+  let speedInput = Number(input[0]);
+
+  if (speedInput <= 10) {
+    console.log("slow");
+  } else if (speedInput <= 50) {
+    console.log("average");
+  } else if (speedInput <= 150) {
+    console.log("fast");
+  } else if (speedInput <= 1000) {
+    console.log("ultra fast");
+  } else {
+    console.log("extremely fast");
+  }
+}
+speed(["49.5"]);
