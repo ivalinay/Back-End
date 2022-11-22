80577c2ba1a55fb5a5be7b93df17d813d4acf1ea
ivalina <ivalina757@gmail.com>
2022-11-22 18:32:52 +0200

    java script elementary project solved

---
+++ javascript/Nov-2022/03.Conditions/numbers.js
@@ -0,0 +1,12 @@
+function numbers(input) {
+  let svetliox = Number(input[0]);
+
+  if (svetlio < 100) {
+    console.log("Less than 100");
+  } else if (svetlio >= 100 && svetlio <= 200) {
+    console.log("Between 100 and 200");
+  } else {
+    console.log("Greater than 200");
+  }
+}
+numbers(["210"]);
