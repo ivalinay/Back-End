3812de274e9109cfe6d2d9efbf55c81695ba3651
ivalina <ivalina757@gmail.com>
2023-01-03 13:46:53 +0200

    GuessANumber game project fundamentals

---
+++ fundamentals/09.project Guess A Number/GuessANumber/guessANumber.js
@@ -0,0 +1,32 @@
+function guessANumber() {
+  const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  let computerGuess = Math.floor(Math.random() * 100);
+
+  let recursiveAsyncReadLine = function () {
+    readline.question("Guess the number (0-100): ", (number) => {
+      let guess = Number(number);
+
+      if (guess <= 100 && guess >= 0) {
+        if (guess == computerGuess) {
+          console.log("You guess it!");
+          return readline.close();
+        } else if (guess < computerGuess) {
+          console.log("Too Low");
+        } else if (guess > computerGuess) {
+          console.log("Too High");
+        }
+      } else {
+        console.log("Invalid input! Try again...");
+      }
+
+      recursiveAsyncReadLine();
+    });
+  };
+  recursiveAsyncReadLine();
+}
+
+guessANumber();
