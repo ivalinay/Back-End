c1f879c31b95f59b7316f1e7ae80d93c8b56b36b
ivalina <ivalina757@gmail.com>
2023-01-18 12:23:48 +0200

    Solved exercise Arrays Advanced javaScript

---
+++ fundamentals/18.Exercise Arrays Advanced/02.distinctArray.js
@@ -0,0 +1,12 @@
+function distinctArrays(arr) {
+  let uniqueNumbersArray = [];
+  let arrayLength = arr.length;
+
+  for (let index = 0; index < arrayLength; index++) {
+    if (!uniqueNumbersArray.includes(arr[index])) {
+      uniqueNumbersArray.push(arr[index]);
+    }
+  }
+  console.log(uniqueNumbersArray.join(" "));
+}
+distinctArrays([7, 8, 9, 7, 2, 3, 4, 1, 2]);
