506ff11de66809c3936ac45e772dfb9bcfa6f478
ivalina <ivalina757@gmail.com>
2023-08-08 12:20:39 +0300

    JS Applications Samurider

---
+++ Javascript Applications/06.Routing - Exercise/09.examPrep/Sole Mates_Resources/src/components/login/login.js
@@ -0,0 +1,37 @@
+export class LoginComponent {
+  constructor(authService, renderHandler, templateFunction, router) {
+    this.authService = authService;
+    this.renderHandler = renderHandler;
+    this.templateFunction = templateFunction;
+    this.router = router;
+    this.loginHandler = this._loginHandler.bind(this);
+    this.showView = this._showView.bind(this);
+  }
+  async _showView() {
+    let template = this.templateFunction(this.loginHandler);
+    this.renderHandler(template);
+  }
+  async _loginHandler(e) {
+    console.log("test");
+    e.preventDefault();
+
+    let form = e.target;
+    let formData = new FormData(form);
+
+    let email = formData.get("email");
+    let password = formData.get("password");
+
+    if (email == "" || password == "") {
+      alert("Email and password cannot be empty.");
+      return;
+    }
+
+    let user = { email, password };
+    try {
+      let result = await this.authService.login(user);
+      this.router.navigate("/dashboard");
+    } catch (e) {
+      alert(e.message);
+    }
+  }
+}
