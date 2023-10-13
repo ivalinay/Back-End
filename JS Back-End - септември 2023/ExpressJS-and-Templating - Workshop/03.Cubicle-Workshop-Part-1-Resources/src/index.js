6603389aa2d186947666f1205a2dbb5a21c3998b
ivalina <ivalina757@gmail.com>
2023-10-13 18:29:23 +0300

    CUP-P1 Modular routing,inited home and cube controlles,fixed href in  navigation for create

--- JS Back-End - септември 2023/ExpressJS-and-Templating - Workshop/03.Cubicle-Workshop-Part-1-Resources/src/index.js
+++ JS Back-End - септември 2023/ExpressJS-and-Templating - Workshop/03.Cubicle-Workshop-Part-1-Resources/src/index.js
@@ -2,7 +2,8 @@
 const express = require("express");
 const handlebarsConfig = require("./config/handlebarsConfig");
 const expressConfig = require("./config/expressConfig");
-const { PORT } = require('./constans')
+const { PORT } = require("./constans");
+const routes = require("./router");
 
 // Lacal variables
 const app = express();
@@ -11,9 +12,7 @@ const app = express();
 expressConfig(app);
 handlebarsConfig(app);
 
-// Routing 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+// Routing
+app.use(routes);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
