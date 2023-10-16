06a87129a089f22117ff15c7cc986e32f1d375d3
ivalina <ivalina757@gmail.com>
2023-10-16 18:44:16 +0300

    CUP-P1 Implement the search functionality in the home page

--- JS Back-End - септември 2023/ExpressJS-and-Templating - Workshop/03.Cubicle-Workshop-Part-1-Resources/src/services/cubeService.js
+++ JS Back-End - септември 2023/ExpressJS-and-Templating - Workshop/03.Cubicle-Workshop-Part-1-Resources/src/services/cubeService.js
@@ -4,31 +4,28 @@ const cubes = [
     id: "un0jndnqlmtg3mgn",
     name: "Kotkata Pesho",
     description: "asd",
-    imageUrl: "htpps://images.pexels.com/photos/1500610/pexels-photo-1500610.jpeg?cs=srgb&dl=pexels-jadson-thomas-1500610.jpg&fm=jpg",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Scramble.svg/800px-Scramble.svg.png",
     difficultyLevel: 1,
   },
   {
     id: "un0jndnqlmtg3rm3",
     name: "sad",
     description: "asd",
-    imageUrl: "asd",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Scramble.svg/800px-Scramble.svg.png",
     difficultyLevel: 1,
   },
   {
     id: "un0jndnqlmtg4are",
     name: "cube3",
     description: "n/a",
-    imageUrl: "htpps://images.pexels.com/photos/1500610/pexels-photo-1500610.jpeg?cs=srgb&dl=pexels-jadson-thomas-1500610.jpg&fm=jpg",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Scramble.svg/800px-Scramble.svg.png",
     difficultyLevel: 3,
   },
 ];
 
 exports.create = (cubeData) => {
-  const id = uniqid();
-  console.log({ id });
-
   const newCube = {
-    id,
+    id: uniqid(),
     ...cubeData,
   };
 
@@ -36,6 +33,20 @@ exports.create = (cubeData) => {
   return newCube;
 };
 
-exports.getAll = () => {
-  return [...cubes];
+exports.getAll = (search, from, to) => {
+  let filterCubes = [...cubes];
+  if (search) {
+    filterCubes = filter.filter((cube) => cube.name.toLowerCase().includes(search.toLowerCase()));
+  }
+  if (from) {
+    filterCubes = filterCubes.filter((cube) => cube.difficultyLevel >= Number(from));
+  }
+  if (to) {
+    filterCubes = filterCubes.filter((cube) => cube.difficultyLevel <= Number(to));
+  }
+  return filterCubes;
+};
+
+exports.getSingleCube = (id) => {
+  return cubes.find((cube) => cube.id === id);
 };
