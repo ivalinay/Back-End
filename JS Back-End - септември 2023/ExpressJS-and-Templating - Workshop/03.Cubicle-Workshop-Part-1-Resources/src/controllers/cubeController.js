b93e861a1c91ae0f84296ada06af691fa60627cc
ivalina <ivalina757@gmail.com>
2023-10-16 18:43:12 +0300

    CUP-P1 Implement details page, provide error handling for wrong id.

--- JS Back-End - септември 2023/ExpressJS-and-Templating - Workshop/03.Cubicle-Workshop-Part-1-Resources/src/controllers/cubeController.js
+++ JS Back-End - септември 2023/ExpressJS-and-Templating - Workshop/03.Cubicle-Workshop-Part-1-Resources/src/controllers/cubeController.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const cubeService = require("../services/cubeService");
 
 router.get("/create", (req, res) => {
-  console.log(cubeServive.getAll());
   res.render("create");
 });
 
@@ -10,12 +9,22 @@ router.post("/create", (req, res) => {
   const { name, description, imageUrl, difficultyLevel } = req.body;
 
   cubeService.create({
-      name,
-      description,
-      imageUrl, 
-      difficultyLevel: Number(difficultyLevel) 
-    });
+    name,
+    description,
+    imageUrl,
+    difficultyLevel: Number(difficultyLevel),
+  });
   res.redirect("/");
 });
 
+router.get("/:cubeId/details", (req, res) => {
+  const { cubeId } = req.params;
+  const cube = cubeService.getSingleCube(cubeId);
+  if (!cube) {
+    res.redirect("/404");
+    return;
+  }
+
+  res.render("details", { ...cube });
+});
 module.exports = router;
