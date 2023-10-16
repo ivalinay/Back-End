c3738360ac972bb68214c7bb51dc4b495c101c2d
ivalina <ivalina757@gmail.com>
2023-10-16 17:15:20 +0300

    CUP-P1 Add bodyparser,provide cube create functionality,add cube template and foreache it

--- JS Back-End - септември 2023/ExpressJS-and-Templating - Workshop/03.Cubicle-Workshop-Part-1-Resources/src/config/handlebarsConfig.js
+++ JS Back-End - септември 2023/ExpressJS-and-Templating - Workshop/03.Cubicle-Workshop-Part-1-Resources/src/config/handlebarsConfig.js
@@ -1,6 +1,5 @@
 const handlebars = require("express-handlebars");
 
-
 const handlebarsConfig = (app) => {
   app.engine("hbs", handlebars.engine({ extname: "hbs" }));
   app.set("view engine", "hbs");
