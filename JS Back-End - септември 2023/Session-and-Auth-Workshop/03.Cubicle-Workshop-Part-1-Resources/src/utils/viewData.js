764a36891e6e3e0e1a167c8ca866598be30b2389
ivalina <ivalina757@gmail.com>
2023-12-13 17:41:39 +0700

    Add util for generation of options,map,edit and delete templates to real data

---
+++ JS Back-End - септември 2023/Session-and-Auth-Workshop/03.Cubicle-Workshop-Part-1-Resources/src/utils/viewData.js
@@ -0,0 +1,13 @@
+exports.difficultyLevelOptionsViewData = (difficultyLevel) => {
+  const titles = ["Easy", "Medium (Standard 3x3) ", "Intermediate", "Expert", "Hardcode"];
+
+  const options = titles.map((title, index) => {
+    const value = index + 1;
+    return {
+      title: `${value} - ${title}`,
+      value,
+      selected: Number(difficultyLevel) === value,
+    };
+  });
+  return options;
+};
