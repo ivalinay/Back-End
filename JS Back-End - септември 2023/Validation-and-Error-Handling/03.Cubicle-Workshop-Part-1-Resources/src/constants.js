3d3cad0bbddc776ca5d1349e1a304300ac83a8fb
ivalina <ivalina757@gmail.com>
2024-01-03 21:48:43 +0100

    Validation and Error Handling

---
+++ JS Back-End - септември 2023/Validation-and-Error-Handling/03.Cubicle-Workshop-Part-1-Resources/src/constants.js
@@ -0,0 +1,9 @@
+const constants = {
+  PORT: 5050,
+
+  //         localhost = 127.0.0.1
+  URL: "mongodb://127.0.0.1:27017/cubical-sep-2023", // Connection String;
+  SECRET: "c640e6d6-3d06-4291-b2ad-02dcb6ac25ef",
+};
+
+module.exports = constants;
---
+++ JS Back-End - септември 2023/Validation-and-Error-Handling/demo/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+
+const PORT = 5050;
+
+app.get("/", (req, res) => {
+  res.send(`
+  <form method="POST">
+    <label for="name">Name</label>
+    <input type="name" name="name" id="name" />
+
+    <label for="password">Password</label>
+    <input type="password" name="password" id="password" />
+
+  <input type="submit" name="value" />
+</form>
+  `);
+});
+
+app.post("/", (req, res) => {
+  const { name, password } = req.body;
+  console.log(name, password);
+
+  if (!name || name.length <= 4) {
+    return res.status(400).send("Invalid name!");
+  }
+
+  if (!password || password.length < 5) {
+    return res.status(400).send("Invalid password");
+  }
+
+  res.send("Ok!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT} ...`);
+});
