1dcc6fcf95bce58408912d5123b2a7cf8320be27
ivalina <ivalina757@gmail.com>
2024-01-12 17:40:59 +0100

    Add Error Handling to cubical app

--- JS Back-End - септември 2023/Validation-and-Error-Handling/03.Cubicle-Workshop-Part-1-Resources/src/middlewares/errorHandlerMiddleware.js
+++ JS Back-End - септември 2023/Validation-and-Error-Handling/03.Cubicle-Workshop-Part-1-Resources/src/middlewares/errorHandlerMiddleware.js
@@ -2,6 +2,5 @@ const { extractErrorMsgs } = require("../utils/errorHandle");
 
 module.exports = (err, req, res, next) => {
   const errorMessages = extractErrorMsgs(err);
- console.log({errorMsgFromMiddleware: errorMessages});
   res.render('404', {errorMessages})
 };
