fbbdf1cb7376025f07ffe36eb90c975a5d0f2774
ivalina <ivalina757@gmail.com>
2024-01-17 09:23:00 +0100

    Add router post

--- JS Back-End - септември 2023/Workshop REST/server/src/controllers/userController.js
+++ JS Back-End - септември 2023/Workshop REST/server/src/controllers/userController.js
@@ -1,3 +1,8 @@
 const router = require("express").Router();
 
+router.post('/register', (req,res)=>{
+    console.log(req.body);
+    res.send('Ok')
+})
+
 module.exports = router;
--- JS Back-End - септември 2023/Workshop REST/server/src/index.js
+++ JS Back-End - септември 2023/Workshop REST/server/src/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const routes = require("./routes");
 
 const app = express();
 const PORT = 3030;
+
 console.log("works!");
 
-app.get('/',(req,res,next)=>{
-    res.send('Hello from RESTful API!')
-})
+app.get("/", (req, res, next) => {
+  res.send("Hello from RESTful API!");
+});
+
+app.use(routes);
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
