506a9118a51adb8db3aa8e6a00aefac4dedd38c7
ivalina <ivalina757@gmail.com>
2024-01-21 20:47:02 +0200

    Add bcrypt - name and password

--- JS Back-End - септември 2023/Workshop REST/server/src/models/User.js
+++ JS Back-End - септември 2023/Workshop REST/server/src/models/User.js
@@ -1,7 +1,15 @@
-const mongoose = require('mongoose');
- 
-const userSchema =  new mongoose.Schema({})
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const User =  mongoose.model('User', userSchema)
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+userSchema.pre("save", async function () {
+  const hash = await bcrypt.hash(this.password, 10);
+});
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
