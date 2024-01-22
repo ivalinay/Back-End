89bbb61b98b496c7cad07142081fb031e903247a
ivalina <ivalina757@gmail.com>
2024-01-22 19:15:08 +0200

    Express Auth API

--- JS Back-End - септември 2023/Workshop REST/server/src/controllers/userController.js
+++ JS Back-End - септември 2023/Workshop REST/server/src/controllers/userController.js
@@ -1,11 +1,14 @@
 const router = require("express").Router();
-const userService = require('../services/userService')
+const userService = require("../services/userService");
 
-router.post('/register',async (req,res)=>{
+router.post("/register", async (req, res) => {
+  try {
     const { email, password } = req.body;
-
-    await userService.register({ email, password})
-    res.send('Ok')
-})
+    await userService.register({ email, password });
+    res.json({ message: "Ok" });
+  } catch ({ message }) {
+    res.status(400).json({ message });
+  }
+});
 
 module.exports = router;
