b8a5e147c2b4c653b9e9159dac6b617a75ffeb99
ivalina <ivalina757@gmail.com>
2024-01-23 18:05:20 +0200

    Configuration-Error while connecting to the DB

--- JS Back-End - септември 2023/Workshop REST/server/src/index.js
+++ JS Back-End - септември 2023/Workshop REST/server/src/index.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const routes = require("./router");
 
 const app = express();
 const PORT = 3030;
 
-
 // DB Configuration
-mongoose.connect('mongodb://127.0.0.1:27017/furnitures')
-.then(()=> console.log(`Successfully connected to DB!`));
+mongoose
+  .connect("mongodb://127.0.0.1:27017/furnitures")
+  .then(() => console.log(`Successfully connected to the DB!`))
+  .then((err) => console.log(`Error while connecting to the DB!`, err));
 
 //Middleware Configurations
-app.use(express.urlencoded({extended: false })) // urlencoded, querystring
+app.use(express.urlencoded({ extended: false })); // urlencoded, querystring
 app.use(express.json()); // application/json -> AJAX requests
-app.use(cors())
+app.use(cors());
 
 // app.use((req, res, next) => {
 //   res.setHeader("Access-Control-Allow-Orgin", "*");
