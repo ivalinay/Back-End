3d220994fbefd5337c02d51871f500db768cc32a
ivalina <ivalina757@gmail.com>
2024-01-24 14:42:06 +0200

    Add JWT token generation with user payload and expiration

--- JS Back-End - септември 2023/Workshop REST/server/src/services/userService.js
+++ JS Back-End - септември 2023/Workshop REST/server/src/services/userService.js
@@ -1,19 +1,24 @@
-const User = require('../models/User')
-const bcrypt = require('bcrypt')
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-exports.register = (userData) => User.create(userData)
+exports.register = (userData) => User.create(userData);
 
 exports.login = async (userData) => {
-    const user = await User.findOne({email})
+  const user = await User.findOne({ email });
 
-    if(!user){
-        throw new Error('Invalid email or password')
-    }
+  if (!user) {
+    throw new Error("Invalid email or password");
+  }
 
-    const isValid =  await bcrypt.compare(password,user.password)
+  const isValid = await bcrypt.compare(password, user.password);
 
-        if (!isValid) {
-          throw new Error("Invalid email or password");
-        }
+  if (!isValid) {
+    throw new Error("Invalid email or password");
+  }
+  const payload = {_id: user._id, email: user.email};
+  const token = jwt.sign(payload, "SOME_SECRET", { expiresIn: "2d" }); // 2d too much
 
-}
+
+
+};
