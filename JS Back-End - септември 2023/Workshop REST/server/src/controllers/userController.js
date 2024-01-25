dd4a823042775eb2d6699b719f1bc25b94002423
ivalina <ivalina757@gmail.com>
2024-01-25 21:54:45 +0200

    Express-JWT-Auth-Middleware

--- JS Back-End - септември 2023/Workshop REST/server/src/controllers/userController.js
+++ JS Back-End - септември 2023/Workshop REST/server/src/controllers/userController.js
@@ -4,9 +4,9 @@ const userService = require("../services/userService");
 router.post("/register", async (req, res) => {
   try {
     const { email, password } = req.body;
-    await userService.register({ email, password });
-
-    res.json({ message: "Registered Successfully!" });
+   const result = await userService.register({ email, password });
+   
+   res.json(result);
   } catch ({ message }) {
     res.status(400).json({ message });
   }
@@ -16,10 +16,15 @@ router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
     const result = await userService.login({ email, password });
+   
     res.json({ result });
   } catch ({ message }) {
     res.status(400).json({ message });
   }
 });
+
+router.get('/logout',(req, res)=>{
+  res.end() //TODO: check if the tokenis valid 
+})
 
 module.exports = router;
