0c7aa1bc72a3e501e5bdcdc6be29c95069ae6ec9
ivalina <ivalina757@gmail.com>
2024-01-25 13:41:42 +0200

    Implement getResult function for JWT generation

--- JS Back-End - септември 2023/Workshop REST/server/src/services/userService.js
+++ JS Back-End - септември 2023/Workshop REST/server/src/services/userService.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 
 exports.register = (userData) => User.create(userData);
 
-exports.login = async (userData) => {
+exports.login = async ({ email, password }) => {
   const user = await User.findOne({ email });
 
   if (!user) {
@@ -16,6 +16,12 @@ exports.login = async (userData) => {
   if (!isValid) {
     throw new Error("Invalid email or password");
   }
+
+  const result = getResult(user);
+  return result;
+};
+
+function getResult(user) {
   const payload = { _id: user._id, email: user.email };
   const token = jwt.sign(payload, "SOME_SECRET", { expiresIn: "2d" }); // 2d too much
 
@@ -24,6 +30,5 @@ exports.login = async (userData) => {
     accessToken: token,
     email: user.email,
   };
-  
   return result;
-};
+}
