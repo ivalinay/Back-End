bcb7e024fd3738a21c6cd5cb3a18e326f563e803
ivalina <ivalina757@gmail.com>
2024-01-26 16:01:15 +0200

    Add POST route for creating furniture

--- JS Back-End - септември 2023/Workshop REST/server/src/controllers/furnitureController.js
+++ JS Back-End - септември 2023/Workshop REST/server/src/controllers/furnitureController.js
@@ -9,4 +9,24 @@ router.get("/", async (req, res) => {
     res.status(400).json({ message });
   }
 });
-a
+
+router.post("/", async (req, res) => {
+  try {
+    const { description, img, make, material, model, price, year } = req.body;
+    await furnitureService.create({
+         description,
+         img,
+         make,
+         material,
+         model,
+         price,
+         year
+         });
+         
+         res.status(201).end()
+  } catch ({ message }) {
+    res.status(400).json({ message });
+  }
+});
+
+module.exports = router;
