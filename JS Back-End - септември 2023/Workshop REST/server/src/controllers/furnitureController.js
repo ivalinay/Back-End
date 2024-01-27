29ca2b03a2db4112404b924f988ad2c4ea7d5307
ivalina <ivalina757@gmail.com>
2024-01-27 19:42:16 +0200

    Improve code structure in Furniture model

--- JS Back-End - септември 2023/Workshop REST/server/src/controllers/furnitureController.js
+++ JS Back-End - септември 2023/Workshop REST/server/src/controllers/furnitureController.js
@@ -12,21 +12,28 @@ router.get("/", async (req, res) => {
 
 router.post("/", async (req, res) => {
   try {
+    console.log(req.user);
     const { description, img, make, material, model, price, year } = req.body;
     await furnitureService.create({
-         description,
-         img,
-         make,
-         material,
-         model,
-         price,
-         year
-         });
-         
-         res.status(201).end()
+      description,
+      img,
+      make,
+      material,
+      model,
+      price,
+      year,
+      _ownerId: req.user._id,
+    });
+
+    res.status(201).end();
   } catch ({ message }) {
     res.status(400).json({ message });
   }
+});
+
+router.get("/:furnitureId", async (req, res) => {
+  const { furnitureId } = req.params;
+  await furnitureService.getOne(furnitureId);
 });
 
 module.exports = router;
